0e428dc8357c72cd03f92d57f20d66d0d809f75a
GuilhermeVoyna <guilhermeguerr4@gmail.com>
2025-08-26 19:12:47 -0300

    String Json to JsonObject

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,35 @@
 package org.example;
 
 
-import org.example.utils.json.JsonParameter;
+import org.example.utils.json.JsonObject;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 
 public class Main {
         public static void main(String[] args) throws IOException {
 
             String sJson = """
 {
-  "user": {
-    "id": "123",
-    "name": "Alice",
-    "settings": {
-      "theme": {
-        "sempre": "true"
+  "A" : {
+    "B"  : 123    ,
+    "C": "Alice",
+    "D" : {
+      "E" : {
+        "F"  :  true
       },
-      "notifications": {
-        "email": "true",
-        "sms": "false"
+      "G": {
+        "H": true   ,
+        "I": null
       }
     }
   },
-  "active": "true"
+  "J": "true"
 }
 
 """;
 
             System.out.println(sJson);
-            JsonParameter.toObject(sJson);
+            JsonObject.toObject(sJson);
         }
 
 }
--- src/main/java/org/example/utils/json/JsonParser.java
+++ src/main/java/org/example/utils/json/JsonParser.java
@@ -1,5 +1,7 @@
 package org.example.utils.json;
 
+import java.util.HashMap;
+
 public class JsonParser {
 
     public static <T> String toJson(T instance){
@@ -8,7 +10,9 @@ public class JsonParser {
     /*
     * Converts a json into an object
     * */
-   // public static <T> T fromJson(String sJson,Class<T> tClass){
-       // JsonObject json = new JsonObject(sJson);}
+   public static <T> T fromJson(String sJson,Class<T> tClass){
+      HashMap<String,JsonObject> json = JsonObject.toObject(sJson);
+      return null;
+   }
 
 }
